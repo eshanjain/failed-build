2281bdff6c1672e43017eaccfc2c2cfd2c6835ad
eshanjain <eshanjain1000@gmail.com>
2025-02-13 11:36:53 -0800

    Update basicOperations.test.js

--- _tests_/calculator/basicOperations.test.js
+++ _tests_/calculator/basicOperations.test.js
@@ -3,7 +3,7 @@ import { add, subtract, multiply, divide, random } from '../../src/calculator/ba
 
 describe('Basic Calculator Operations', () => {
   test('addition works correctly', () => {
-    expect(add(2, 3)).toBe(4);
+    expect(add(2, 3)).toBe(5);
     expect(add(-1, 1)).toBe(0);
     expect(add(0, 0)).toBe(0);
   });
